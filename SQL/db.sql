633718a9d1817b2823ebd0381695790ff1ac9a3d
Coimbra <workingcoimbra1607@gmail.com>
2025-11-27 13:44:37 -0400

    Feat: create new triggers and procedures

--- SQL/db.sql
+++ SQL/db.sql
@@ -36,3 +36,40 @@ quantidade_cartas int unsigned,
 foreign key (pedidos_id_pedidos) references pedidos(id_pedidos),
 foreign key (cartas_id_cartas) references cartas(id_cartas)
 );
+
+delimiter $$
+
+create trigger trg_before_insert_pedidos_cartas
+before insert on pedidos_cartas
+for each row
+begin
+    if (select quantidade from cartas where id_cartas = new.cartas_id_cartas) < new.quantidade_cartas then
+        signal sqlstate '45000'
+        set message_text = 'Estoque insuficiente para a carta selecionada.';
+    end if;
+end$$
+
+create trigger trg_after_insert_pedidos_cartas
+after insert on pedidos_cartas
+for each row
+begin
+    update cartas
+    set quantidade = quantidade - new.quantidade_cartas
+    where id_cartas = new.cartas_id_cartas;
+end$$
+
+delimiter ;
+
+delimiter $$
+
+create procedure criar_login(
+in p_nome varchar(180),
+in p_usuario varchar(100),
+in p_senha varchar(32),
+in p_email varchar(255)
+)
+begin
+insert into login(nome, usuario, senha, email) values(p_nome, p_usuario, p_senha, p_email);
+end$$
+
+delimiter ;
